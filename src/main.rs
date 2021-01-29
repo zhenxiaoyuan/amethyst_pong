cc38b0e8d3e8444e10c442694009f20b4f877792
hao.zhen <i_zhenhao@163.com>
2021-01-29 21:27:00 +0800

    First commit.

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+mod pong;
+
+use crate::pong::Pong;
+
+use amethyst::{prelude::*, renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    }, utils::{
+        application_dir, application_root_dir
+    }, core::transform::TransformBundle};
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+    let display_config_path = app_root.join("config").join("display.ron");
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config_path)?
+                        .with_clear([0.0, 0.0, 0.0, 1.0]),
+                )
+                .with_plugin(RenderFlat2D::default()))?
+        .with_bundle(TransformBundle::new())?;
+
+    let assets_dir = app_root.join("assets");
+    let mut game = Application::new(assets_dir, Pong, game_data)?;
+
+    game.run();
+
+    Ok(())
+}
