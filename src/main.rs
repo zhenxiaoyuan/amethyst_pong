e65f0cef43fc68a8007770ea91f9a6dafc310d04
hao.zhen <i_zhenhao@163.com>
2021-02-05 15:28:15 +0800

    Add ball and make it move, add collision and make them bounce.

--- src/main.rs
+++ src/main.rs
@@ -38,11 +38,16 @@ fn main() -> amethyst::Result<()> {
                 .with_plugin(RenderFlat2D::default()))?
         .with_bundle(TransformBundle::new())?
         .with_bundle(input_bundle)?
-        .with(systems::PaddleSystem, "paddle_system", &["input_system"]
-    );
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"])
+        .with(systems::MoveBallsSystem, "ball_system", &[])
+        .with(
+            systems::BounceSystem,
+            "collision_system",
+            &["paddle_system", "ball_system"],
+        );
 
     let assets_dir = app_root.join("assets");
-    let mut game = Application::new(assets_dir, Pong, game_data)?;
+    let mut game = Application::new(assets_dir, Pong::default(), game_data)?;
 
     game.run();
 
--- src/pong.rs
+++ src/pong.rs
@@ -1,6 +1,6 @@
 use amethyst::{
         assets::{AssetStorage, Loader, Handle},
-        core::transform::Transform, 
+        core::{transform::Transform, timing::Time}, 
         ecs::{Component, DenseVecStorage}, 
         prelude::*, 
         renderer::{Camera, ImageFormat, SpriteRender, 
@@ -14,16 +14,63 @@ pub const ARENA_WIDTH: f32 = 100.0;
 pub const PADDLE_HEIGHT: f32 = 16.0;
 pub const PADDLE_WIDTH: f32 = 4.0;
 
-pub struct Pong;
+pub const BALL_VELOCITY_X: f32 = 10.0;
+pub const BALL_VELOCITY_Y: f32 = 5.0;
+pub const BALL_RADIUS: f32 = 2.0;
+
+#[derive(Default)]
+pub struct Pong {
+    ball_spawn_timer: Option<f32>,
+    sprite_sheet_handle: Option<Handle<SpriteSheet>>,
+}
 
 impl SimpleState for Pong {
-    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
-        let world = _data.world;
-        let sprite_sheet_handle = load_sprite_sheet(world);
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+
+        self.ball_spawn_timer.replace(1.0);
+        self.sprite_sheet_handle.replace(load_sprite_sheet(world));
 
         initialise_camera(world);
-        initialise_paddles(world, sprite_sheet_handle);
+        initialise_paddles(world, self.sprite_sheet_handle.clone().unwrap());
     }
+
+    fn update(&mut self, data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
+        if let Some(mut timer) = self.ball_spawn_timer.take() {
+            {
+                let time = data.world.fetch::<Time>();
+                timer -= time.delta_seconds();
+            }
+            if timer <= 0.0 {
+                initialise_ball(data.world, self.sprite_sheet_handle.clone().unwrap());
+            } else {
+                self.ball_spawn_timer.replace(timer);
+            }
+        }
+        Trans::None
+    }
+}
+
+fn load_sprite_sheet(world: &mut World) -> Handle<SpriteSheet> {
+    let texture_handle = {
+        let loader = world.read_resource::<Loader>();
+        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+        loader.load(
+            "texture/pong_spritesheet.png",
+            ImageFormat::default(), 
+            (), 
+            &texture_storage,
+        )
+    };
+
+    let loader = world.read_resource::<Loader>();
+    let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
+    loader.load(
+        "texture/pong_spritesheet.ron",
+        SpriteSheetFormat(texture_handle),
+        (),
+        &sprite_sheet_store,
+    )
 }
 
 fn initialise_camera(world: &mut World) {
@@ -88,24 +135,28 @@ fn initialise_paddles(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet
         .build();
 }
 
-fn load_sprite_sheet(world: &mut World) -> Handle<SpriteSheet> {
-    let texture_handle = {
-        let loader = world.read_resource::<Loader>();
-        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
-        loader.load(
-            "texture/pong_spritesheet.png",
-            ImageFormat::default(), 
-            (), 
-            &texture_storage,
-        )
-    };
+pub struct Ball {
+    pub velocity: [f32; 2],
+    pub radius: f32,
+}
 
-    let loader = world.read_resource::<Loader>();
-    let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
-    loader.load(
-        "texture/pong_spritesheet.ron",
-        SpriteSheetFormat(texture_handle),
-        (),
-        &sprite_sheet_store,
-    )
+impl Component for Ball {
+    type Storage = DenseVecStorage<Self>;
+}
+
+fn initialise_ball(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
+    let mut local_transform = Transform::default();
+    local_transform.set_translation_xyz(ARENA_WIDTH / 2.0, ARENA_HEIGHT / 2.0, 0.0);
+
+    let sprite_render = SpriteRender::new(sprite_sheet_handle, 1);
+
+    world
+        .create_entity()
+        .with(sprite_render)
+        .with(Ball {
+            radius: BALL_RADIUS,
+            velocity: [BALL_VELOCITY_X, BALL_VELOCITY_Y],
+        })
+        .with(local_transform)
+        .build();
 }
